0d55c1bf05edd35e105065fb63a63f39904f1cca
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2023-12-28 10:07:49 +0330

    [UPDATE] content component

--- minitodomal/src/componenets/Content.jsx
+++ minitodomal/src/componenets/Content.jsx
@@ -1,6 +1,17 @@
 import './Content.scss';
 function Content() {
-  return <div className={'content'}>Content</div>;
+  return (
+    <div className={'content'}>
+      <button>Add task</button>
+      <div>
+        <ul>
+          <li>task 1</li>
+          <li>task 2</li>
+          <li>task 3</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Content;
